f4817d0efa50b8e874a24eda3c59bad969b0635f
EvolvDK <elouarn.de.kerros@efrei.net>
2025-08-11 20:04:16 +0200

    feat: implement Youtube events

---
+++ src/shared/events.rs
@@ -0,0 +1,140 @@
+use std::{path::PathBuf, time::Duration};
+
+use anyhow::Result;
+use crossterm::event::KeyEvent;
+use serde::{Deserialize, Serialize};
+
+use super::{
+    ipc::ipc_stream::IpcStream,
+    lrc::{LrcIndex, LrcIndexEntry},
+    mouse_event::MouseEvent,
+    mpd_query::{MpdCommand, MpdQuery, MpdQueryResult, MpdQuerySync},
+};
+use crate::{
+    config::{
+        Config,
+        Size,
+        cli::{Command, RemoteCommandQuery},
+        tabs::PaneType,
+        theme::UiConfig,
+    },
+    mpd::commands::IdleEvent,
+    ui::UiAppEvent,
+    youtube::YouTubeVideo,
+};
+
+#[derive(Debug, Clone)]
+pub(crate) struct RefreshContext {
+    pub old_song_id: u32,
+    pub position: usize,
+}
+
+#[derive(Debug)]
+#[allow(unused)]
+pub(crate) enum ClientRequest {
+    Query(MpdQuery),
+    QuerySync(MpdQuerySync),
+    Command(MpdCommand),
+}
+
+#[derive(Debug)]
+#[allow(unused)]
+pub(crate) enum WorkRequest {
+    IndexLyrics {
+        lyrics_dir: String,
+    },
+    IndexSingleLrc {
+        /// Absolute path to the lrc file
+        path: PathBuf,
+    },
+    Command(Command),
+    YouTubeSearch {
+        query: String,
+    },
+    GetYouTubeStreamUrl {
+        video: YouTubeVideo,
+        context: Option<RefreshContext>,
+    },
+    YouTubeGetVideoInfo {
+        id: String,
+    },
+}
+
+#[derive(Debug)]
+#[allow(clippy::large_enum_variant)] // the instances are short lived events, its fine.
+pub(crate) enum WorkDone {
+    LyricsIndexed { index: LrcIndex },
+    SingleLrcIndexed { lrc_entry: Option<LrcIndexEntry> },
+    YouTubeSearchResult(YouTubeVideo),
+    YouTubeSearchFinished,
+    YouTubeStreamUrlReady {
+        url: String,
+        video: YouTubeVideo,
+        context: Option<RefreshContext>,
+    },
+    YouTubeStreamUrlFailed {
+        video: YouTubeVideo,
+        context: Option<RefreshContext>,
+    },
+    MpdCommandFinished { id: &'static str, target: Option<PaneType>, data: MpdQueryResult },
+    YouTubeVideoInfoFetched(YouTubeVideo),
+    None,
+}
+
+// The instances are short lived events, boxing would most likely only hurt
+// here.
+#[allow(clippy::large_enum_variant)]
+#[derive(Debug)]
+pub(crate) enum AppEvent {
+    UserKeyInput(KeyEvent),
+    UserMouseInput(MouseEvent),
+    Status(String, Level, Duration),
+    InfoModal {
+        message: Vec<String>,
+        title: Option<String>,
+        size: Option<Size>,
+        replacement_id: Option<String>,
+    },
+    Log(Vec<u8>),
+    IdleEvent(IdleEvent),
+    RequestRender,
+    Resized {
+        columns: u16,
+        rows: u16,
+    },
+    ResizedDebounced {
+        columns: u16,
+        rows: u16,
+    },
+    WorkDone(Result<WorkDone>),
+    UiEvent(UiAppEvent),
+    Reconnected,
+    LostConnection,
+    TmuxHook {
+        hook: String,
+    },
+    ConfigChanged {
+        config: Box<Config>,
+        keep_old_theme: bool,
+    },
+    ThemeChanged {
+        theme: Box<UiConfig>,
+    },
+    RemoteSwitchTab {
+        tab_name: String,
+    },
+    IpcQuery {
+        stream: IpcStream,
+        targets: Vec<RemoteCommandQuery>,
+    },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Copy, Eq, Hash, PartialEq)]
+#[allow(dead_code)]
+pub enum Level {
+    Trace,
+    Debug,
+    Warn,
+    Error,
+    Info,
+}
